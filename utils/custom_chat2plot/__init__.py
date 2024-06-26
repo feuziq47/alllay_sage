e70ed36fafed88fb3456d312e2e3608045ea5db6
Jiyu-Kim <pjy707099@gmail.com>
2024-06-26 07:26:15 +0000

    Change directory structure

--- utils/custom_chat2plot/__init__.py
+++
@@ -1,9 +0,0 @@
-from chat2plot.chat2plot import (  # noQA
-    Chat2Plot,
-    Chat2Vega,
-    Plot,
-    PlotConfig,
-    ResponseType,
-    chat2plot,
-)
-from chat2plot.version import __version__  # noQA
