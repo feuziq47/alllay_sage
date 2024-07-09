1f856836b2719edf02157d09359f77b67483dffe
Jiyu Kim <74771124+Jiyu-Kim@users.noreply.github.com>
2024-07-09 23:07:23 +0900

    Update install_tesseract.sh

--- genai/aws-gen-ai-kr/20_applications/02_qa_chatbot/10_hands_on_lab/02_rag_over_complex_pdf/setup/install_tesseract.sh
+++ genai/aws-gen-ai-kr/20_applications/02_qa_chatbot/10_hands_on_lab/02_rag_over_complex_pdf/setup/install_tesseract.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-
 set -x
-
-nohup bash <<'EOF' &
-
+ 
 echo "## Step 1"
 yum -y update
 yum -y upgrade
@@ -15,18 +12,21 @@ cd leptonica-1.84.1
 ./configure
 make
 make install
-
  
 echo "## Step 2"
 cd ~
 yum install git-core libtool pkgconfig -y
-git clone --depth 1 https://github.com/tesseract-ocr/tesseract.git tesseract-ocr
-cd tesseract-ocr 
+wget https://github.com/tesseract-ocr/tesseract/archive/5.3.1.tar.gz
+tar xzvf 5.3.1.tar.gz
+cd tesseract-5.3.1
+#git clone --depth 1 https://github.com/tesseract-ocr/tesseract.git tesseract-ocr
+#cd tesseract-ocr 
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig
 ./autogen.sh
 ./configure 
 make
 make install
+ldconfig
  
 echo "## Step 3"
 cd /usr/local/share/tessdata
@@ -41,7 +41,3 @@ wget https://github.com/tesseract-ocr/tessdata/raw/main/kor_vert.traineddata
 echo "## Step 4"
 echo "export TESSDATA_PREFIX=/usr/local/share/tessdata" >> ~/.bash_profile
 echo "export TESSDATA_PREFIX=/usr/local/share/tessdata" >> /home/ec2-user/.bash_profile
-
-echo "## Finished"
-
-EOF
